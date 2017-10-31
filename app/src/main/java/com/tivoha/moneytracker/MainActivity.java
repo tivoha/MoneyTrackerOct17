3ca92861d2f20a3587293682a316c6c54fb88ba4
tivoha <tivoha@mail.ru>
2017-10-31 23:11:33 +0200

    change MainActivity.java

--- app/src/main/java/com/tivoha/moneytracker/MainActivity.java
+++ app/src/main/java/com/tivoha/moneytracker/MainActivity.java
@@ -10,4 +10,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
 }
